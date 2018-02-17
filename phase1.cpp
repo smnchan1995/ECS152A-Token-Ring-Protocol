413c8ad758e3e6e1b499a4ac1d3aa6c59ffc64b3
Seza <seza.habibi@gmail.com>
2018-02-17 09:21:53 -0800

    end of 3.4

--- phase1.cpp
+++ phase1.cpp
@@ -9,21 +9,21 @@ using namespace std;
 
 class Event{
   double time;
-  int paketLength;
+  double ServiceTime;
   char type;
 public:
   Event(){
-     setTime(0); setPacketLength(0); setType('n');   // n stands for no type intialization
+     setTime(0); setServiceTime(0); setType('n');   // n stands for no type intialization
   }
   Event(int t,int l, char c){
-     setTime(t); setPacketLength(l); setType(c);
+     setTime(t); setServiceTime(l); setType(c);
   }
   //~Event();
 
   void setTime(double t){time = t;}
   double getTime() const {return time;}
-  void setPacketLength(int l){paketLength = l;}
-  int getPacketLength() const {return paketLength;}
+  void setServiceTime(double l){ServiceTime = l;}
+  int getServiceTime() const {return ServiceTime;}
   void setType(char t){type = t;}
   int getType() const {return type;}
 };
@@ -36,23 +36,26 @@ struct CompareEvent{
 };
 
 double interArrivalTime(int,double);
-int lengthPacket();
-void processArrivelEvent(Event&);
+//int lengthPacket();
+void processArrivelEvent(const Event&);
+void processDepartureEvent(const Event&);
 
-int const BUFEERSIZE = 10;   // it could be any arbitarry number
-double const lambda = 1.0;             //arrival rate
-double const miue =  0.5;             //service rat
+int const MAXBUFEERSIZE = 10;   // it could be any arbitarry number
+double const lambda = 0.5;             //arrival rate
+double const miue =  0.7;             //service rat
 double GlobalTime = 0;            // keep track of time
 std::queue<Event> buffer;
 std::priority_queue<Event*, vector<Event*>, CompareEvent> GEL;
-
+bool server = false;            // if server is false it is busy if not it is free
+int length = 0;          // number of packets in q
+int drop = 0;
 
 int main(){
   srand(time(NULL));
 
 
 
-  int length = 0;          // number of packets in q
+
 
 
   // GEL.push( new Event( 2, 0, 'a') );
@@ -71,7 +74,7 @@ int main(){
   // }
   // cin.get();
 
-  GEL.push(new Event(interArrivalTime(GlobalTime, lambda),lengthPacket(),'a'));
+  GEL.push(new Event(interArrivalTime(GlobalTime, lambda),interArrivalTime(GlobalTime, miue),'a'));
 
 
 
@@ -85,12 +88,39 @@ double interArrivalTime(int GlobalTime, double rate){
   double  negativeExponentialNumber = (-1/rate)*log(1-u);
   return GlobalTime + negativeExponentialNumber;
 }
-int lengthPacket(){
-  return rand()%16 + 1;
-}
-void processArrivelEvent(Event& A){
+// int lengthPacket(){
+//   return rand()%16 + 1;
+// }
+void processArrivelEvent(const Event& A){
   GlobalTime = A.getTime();
-  GEL.push(new Event(interArrivalTime(GlobalTime, lambda),lengthPacket(),'a'));
-
+  GEL.push(new Event(interArrivalTime(GlobalTime, lambda),interArrivalTime(GlobalTime, miue),'a'));
+  //GEL.push(new Event(interArrivalTime(GlobalTime, lambda),lengthPacket(),'a'));
+  if (server){
+    if (length == 0){
+      double departTime = GlobalTime + A.getServiceTime();
+      GEL.push(new Event(departTime, A.getServiceTime(), 'd'));
+    }
+  }
+  else{
+    if (length < MAXBUFEERSIZE){
+      buffer.push(A);
+      length += 1;
+    }
+    else{
+      drop += 1;
+    }
+    //update statistics
+  }
+}
+void processDepartureEvent(const Event& A){
+  GlobalTime = A.getTime();
+  //update statistics
+  length -= 1;
+  if(length > 0){
+     Event B = buffer.front();
+     buffer.pop();
+     double newDepartTime = GlobalTime + B.getServiceTime();
+     GEL.push(new Event(newDepartTime, B.getServiceTime(), 'd'));
+  }
 
 }
